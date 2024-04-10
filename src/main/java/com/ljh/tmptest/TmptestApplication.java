fb4076a5479aa78e41f0243ea7dc5e6afde00bb6
ljh <943497785@qq.com>
2024-04-10 08:56:55 +0800

    测试-导入

---
+++ src/main/java/com/ljh/tmptest/TmptestApplication.java
@@ -0,0 +1,13 @@
+package com.ljh.tmptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmptestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmptestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ljh/tmptest/TmptestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljh.tmptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmptestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
